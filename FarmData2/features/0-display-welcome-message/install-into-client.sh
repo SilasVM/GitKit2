6190ecbb0271d09e726832cbeb49a70ee3f6d26c
Karl R. Wurst <karl@w-sts.com>
2023-01-10 17:33:09 -0500

    Emoji \u00B9 not rendered in Kit-tty Output (#16)
    Closes #12

--- FarmData2/features/0-display-welcome-message/install-into-client.sh
+++ FarmData2/features/0-display-welcome-message/install-into-client.sh
@@ -2,11 +2,11 @@
 
 echo "*********************************************************************"
 printf "\xF0\x9F\x98\xBA\xF0\x9F\x92\xBB Meow, Kit-tty here!\n"
-printf "(I'm in the 'kit' and communicate through your 'tty'\u00B9 - get it? \xF0\x9F\x98\x81)\n"
+printf "(I'm in the 'kit' and communicate through your 'tty'\xC2\xB9 - get it? \xF0\x9F\x98\x81)\n"
 echo
 echo "I am here to help you be successful in the GitKit activities."
 echo "You will hear from me every once in a while when I think you"
 echo "might need some help."
 echo
-printf "\u00B9To learn about the tty: https://itsfoss.com/what-is-tty-in-linux/\n"
+printf "\xC2\xB9To learn about the tty: https://itsfoss.com/what-is-tty-in-linux/\n"
 echo "*********************************************************************"
