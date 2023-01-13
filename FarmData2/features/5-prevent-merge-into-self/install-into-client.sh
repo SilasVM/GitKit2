2f12bb93597f817e5cdca68b59d433a11f315b05
Stoney Jackson <dr.stoney@gmail.com>
2023-01-13 14:58:10 -0500

    feat: prevent merge into self
    Closes #28

---
+++ FarmData2/features/5-prevent-merge-into-self/install-into-client.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## This script is called once after the instance is cloned by a KitClient.
+## It runs in the clone of the instance in a KitClient.
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+
+cat "$SCRIPT_DIR/prevent-merge-into-self.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-merge"
