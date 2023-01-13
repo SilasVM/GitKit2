f8df0bcfd700a1d55ede48357a3c55ba827c5d21
Karl R. Wurst <karl@w-sts.com>
2023-01-13 15:40:42 -0500

    chore: add delint

--- FarmData2/features/5-prevent-clone-inside-another-repo/install-into-instance.sh
+++ FarmData2/features/5-prevent-clone-inside-another-repo/install-into-instance.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-cp "$SCRIPT_DIR/prevent-clone-inside-another-repo.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-clone"
+cat "$SCRIPT_DIR/prevent-clone-inside-another-repo.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-clone"
