271130d6c3f913ecd1109beb22c530e32fdc1db6
Stoney Jackson <dr.stoney@gmail.com>
2023-01-14 12:56:52 -0500

    feat: prevent remote to non-upstream URL

---
+++ FarmData2/features/5-prevent-remote-to-non-upstream/prevent-remote-to-non-upstream.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
+
+if [ "$2" = "add" && "$3" = "upstream" ] ; then
+    source "$SCRIPT_DIR/../5-detect-upstream-clone/upstream-location.sh"
+    UPSTREAM_URL="$(load-upstream-location)"
+    if [ "$4" != "$UPSTREAM_URL" ] ; then
+        echo "*********************************************************************"
+        printf "\xF0\x9F\x98\xBA\xF0\x9F\x92\xBB Meow, Kit-tty here!\n"
+        echo
+        echo "I think you have the wrong URL for `upstream`."
+        echo "One of the following is probably the problem:"
+        echo
+        echo "  * The URL points to your fork."
+        echo "  * The URL points to the original, real project."
+        echo "  * The URL is not an appropriate clone URL (it should end in .git)."
+        echo "  * The URL is not using HTTPS."
+        echo
+        echo "To get the correct URL, in a Web-browser:"
+        echo
+        echo "  1. Navigate to your fork"
+        echo "  2. Click the link near the top that follows `forked from`"
+        echo "  3. Click the `Code` pill."
+        echo "  4. Select `Local` and `HTTPS`"
+        echo "  5. Click the copy button."
+        echo
+        echo "Now try to create the `upstream` remote again."
+        echo "*********************************************************************"
+        exit 1
+    fi
+fi
