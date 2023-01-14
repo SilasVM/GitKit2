36ae9124ee23723a2b8cc6b0389f8f8406e21532
Stoney Jackson <dr.stoney@gmail.com>
2023-01-14 08:53:57 -0500

    remove unused variable

--- FarmData2/features/5-prevent-merge-into-self/install-into-instance.sh
+++ FarmData2/features/5-prevent-merge-into-self/install-into-instance.sh
@@ -4,7 +4,6 @@
 ## It runs in the clone of the instance in a KitClient.
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-PROJECT_DIR="$(git rev-parse --show-toplevel)"
 
 echo "$SCRIPT_DIR/prevent-merge-into-self.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-merge"
 chmod +x "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-merge"
