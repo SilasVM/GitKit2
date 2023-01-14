d85fc6df1b25b6a666000f9f4756b12fdf7bb698
Stoney Jackson <dr.stoney@gmail.com>
2023-01-14 13:22:08 -0500

    fix install

--- FarmData2/features/5-prevent-remote-to-non-upstream/install-into-instance.sh
+++ FarmData2/features/5-prevent-remote-to-non-upstream/install-into-instance.sh
@@ -5,4 +5,4 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-echo "$SCRIPT_DIR/prevent-remote-to-non-upstream.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-remote"
+cat "$SCRIPT_DIR/prevent-remote-to-non-upstream.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-remote"
