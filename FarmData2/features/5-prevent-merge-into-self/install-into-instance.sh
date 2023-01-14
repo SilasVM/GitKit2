d8e8f4a67e09ea9ae023e03cf8e24de1ac82c757
Stoney Jackson <dr.stoney@gmail.com>
2023-01-14 08:51:12 -0500

    install into instance and chmod +x pre-merge

--- FarmData2/features/5-prevent-merge-into-self/install-into-instance.sh
+++ FarmData2/features/5-prevent-merge-into-self/install-into-instance.sh
@@ -6,4 +6,5 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 
-cat "$SCRIPT_DIR/prevent-merge-into-self.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-merge"
+echo "$SCRIPT_DIR/prevent-merge-into-self.sh" >> "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-merge"
+chmod +x "$SCRIPT_DIR/../8-git-shim/sub-shims/pre-merge"
