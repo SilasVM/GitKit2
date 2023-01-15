372202e6d5247e4d712786519aa4feec21cbf913
Stoney Jackson <dr.stoney@gmail.com>
2023-01-15 14:04:21 -0500

    delint

--- FarmData2/features/5-add-round-two-conflicts/pre-install-into-instance.sh
+++ FarmData2/features/5-add-round-two-conflicts/pre-install-into-instance.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
 
-cd "${REPO_DIR}"
+cd "${REPO_DIR}" || exit 1
 
 git switch main
 git switch -c add-round-two-conflicts
