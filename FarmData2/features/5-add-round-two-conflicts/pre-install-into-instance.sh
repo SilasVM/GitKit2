a0383d65e48d6bc1a5d52f528f8e281aef2dcf15
Stoney Jackson <dr.stoney@gmail.com>
2023-01-15 13:17:40 -0500

    don't stash

--- FarmData2/features/5-add-round-two-conflicts/pre-install-into-instance.sh
+++ FarmData2/features/5-add-round-two-conflicts/pre-install-into-instance.sh
@@ -4,9 +4,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 
 cd "${REPO_DIR}"
 
-git stash
 git switch main
 git switch -c add-round-two-conflicts
 git am "${SCRIPT_DIR}"/*.patch  || (echo "WARNING: could not apply patch" && git am --abort)
 git switch main
-git stash pop
