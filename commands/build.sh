260133039c82a13ff3fc2f403b88ab450a5aef5d
Stoney Jackson <dr.stoney@gmail.com>
2023-02-28 23:43:00 -0500

    fix(ci): improve build command

--- commands/build.sh
+++ commands/build.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
+# Identify the directory containing this script.
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-cd "$SCRIPT_DIR/.." || exit
-PROJECT_NAME="$( basename "$( pwd )" )"
-PROJECT_NAME="${PROJECT_NAME,,}"    # lowercase it
+# Move to project root.
+cd "${SCRIPT_DIR}/.."
+
+# Identify the dockerfile and its build context.
+DOCKERFILE="./gitkit-deploy.dockerfile"
+BUILD_CONTEXT="./"
+
+# The pipeline sets IMAGE_NAME.
+# If we're running locally, it's not set.
+# Set it to the project name, in lowercase.
+if [[ -n "$IMAGE_NAME" ]] ; then
+    IMAGE_NAME="$(basename "$(pwd)")"
+    IMAGE_NAME="${IMAGE_NAME,,}"
+fi
 
 if [[ -n "$PIPELINE_BUILDX_OPTIONS" ]] ; then
-    docker buildx build $PIPELINE_BUILDX_OPTIONS \
-        -f "${SCRIPT_DIR}/../gitkit-deploy.dockerfile" \
-        "${SCRIPT_DIR}/.."
+    # local build
+    docker build --tag "$IMAGE_NAME" --file "$DOCKERFILE" "$BUILD_CONTEXT"
 else
-    docker build -t "$PROJECT_NAME" \
-        -f "${SCRIPT_DIR}/../gitkit-deploy.dockerfile" \
-        "${SCRIPT_DIR}/.."
+    # pipeline build
+    docker buildx build $PIPELINE_BUILDX_OPTIONS --file "$DOCKERFILE" "$BUILD_CONTEXT"
 fi
+
