e70bcdbb6ec4dc1cbe5d993e829efe16333be607
11kaizen <snjuguna@worcester.edu>
2023-11-28 10:23:34 -0500

    refactor: changes to paths in kit speific features post-push-install

--- kitSpecificFeatures/7-create-issues/post-push-install-into-instance.sh
+++ kitSpecificFeatures/7-create-issues/post-push-install-into-instance.sh
@@ -4,7 +4,7 @@
 ## initialiazed. It runs in the local clone of the instance.
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-cd "$SCRIPT_DIR"
+cd "${REPO_DIR}"
 
 set -e
 
