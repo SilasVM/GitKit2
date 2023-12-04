4fff593fb011d294db99672310dd954601f9a775
11kaizen <snjuguna@worcester.edu>
2023-12-04 14:28:49 -0500

    refactor: added environment variables to script

--- kitSpecificFeatures/5-add-conflicts-for-merge-conflict-practice/pre-install-into-instance.sh
+++ kitSpecificFeatures/5-add-conflicts-for-merge-conflict-practice/pre-install-into-instance.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 
 cd "${REPO_DIR}" || exit 1
 
+set -e
+
 git switch main
 git switch -c merge-conflict-practice
 git am "${SCRIPT_DIR}"/*.patch || (echo "WARNING: could not apply patch" && git am --abort)
