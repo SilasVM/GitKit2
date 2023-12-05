79dec74fdfe25ea4a8bb2aea0edd9540b4a18291
11kaizen <snjuguna@worcester.edu>
2023-12-04 21:53:28 -0500

    refactor: added environment variables to post commit install into instance

--- kitSpecificFeatures/5-add-conflicts-for-merge-conflict-practice/post-commit-install-into-instance.sh
+++ kitSpecificFeatures/5-add-conflicts-for-merge-conflict-practice/post-commit-install-into-instance.sh
@@ -2,6 +2,8 @@
 
 cd "${REPO_DIR}" || exit 1
 
+set -e
+
 git switch merge-conflict-practice
 git rebase main
 git switch main
--- kitSpecificFeatures/5-add-round-two-conflicts/post-commit-install-into-instance.sh
+++ kitSpecificFeatures/5-add-round-two-conflicts/post-commit-install-into-instance.sh
@@ -2,6 +2,8 @@
 
 cd "${REPO_DIR}" || exit 1
 
+set -e
+
 git switch add-round-two-conflicts
 git rebase main
 git switch main
